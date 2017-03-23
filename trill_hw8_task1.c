8377e46703adc2f0af63f113ec06b8984050f7a5
Robert Christensen <robertchristensen1@mail.weber.edu>
2017-03-23 16:13:46 +0000

    first try

---
+++ trill_hw8_task1.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  trill_hw8_task1.c
+ *       	Usage:  ./trill_hw8_task1.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/23/2017 04:13:06 PM
+ *       Compiler:  gcc -Wall -Werr
+ *         Author:  Robert Christensen (), robertchristensen1@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
