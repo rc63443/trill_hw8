be16fd47e9ad22029d8b692ee15c233aadbf4f18
Julie Collings <juliecollings@mail.weber.edu>
2017-03-23 16:20:19 +0000

    Initial commit

---
+++ trill_hw8_task2.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  trill_hw8_task2.c
+ *       	Usage:  ./trill_hw8_task2.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/23/2017 04:19:43 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Julie Collings (), juliecollings@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Definitions */
+
+
